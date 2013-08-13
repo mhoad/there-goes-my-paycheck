b55c9d2befce1d1ce49d56f59f50d8ef9ca28d75
Mark Hoad <markdhoad@gmail.com>
2013-08-13 18:28:27 +0700

    Footer added and static version of homepage completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,10 @@ Paycheck::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  #config.assets.compile = true
+  #config.assets.compress = true
   config.assets.debug = true
+
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
