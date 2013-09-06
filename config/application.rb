a9f60d1d755b9b3d1beec042678d0c7e929ef044
Mark Hoad <markdhoad@gmail.com>
2013-09-06 19:17:18 +0700

    Added application.yml for application specific variables

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+    config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+    config.merge! config.fetch(Rails.env, {})
+    config.each do |key, value|
+      ENV[key] = value.to_s unless value.kind_of? Hash
+    end
 module Paycheck
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
