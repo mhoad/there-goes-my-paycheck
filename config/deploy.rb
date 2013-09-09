cd12bc8ad9c03eda1c7a1862e34498ab9ae69c7c
Mark Hoad <markdhoad@gmail.com>
2013-09-09 22:20:03 +0700

    Added additional capistrano tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,4 +25,17 @@ namespace :deploy do
  task :restart, :roles => :app, :except => { :no_release => true } do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
  end
+
+ task :setup_config, roles: :app do
+    run "cp -f ~/database.yml #{release_path}/config/database.yml"
+    run "cp -f ~/application.yml #{release_path}/config/application.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+ task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
 end
