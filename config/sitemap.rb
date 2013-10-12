8e920f9ee34b8b77698d451d22948c2b50afb9f4
Mark Hoad <markdhoad@gmail.com>
2013-10-13 00:48:09 +0700

    Updated XML Sitemap logic for all pages to be better organised

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,40 +1,22 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
-
+SitemapGenerator::Sitemap.default_host = "http://giftgeek.io"
+# Set the path for this sitemap
 SitemapGenerator::Sitemap.create do
-  add '/about'
-  add '/submit'
-  add '/privacy'
+  add about_path,   :priority => 0.3, :changefreq => 'monthly'
+  add submit_path,  :priority => 0.1, :changefreq => 'yearly'
+  add privacy_path, :priority => 0.1, :changefreq => 'yearly'
 
-  Category.find_each do |category|
-    add category_path(category), :lastmod => category.updated_at
+  group(:filename => :categories) do
+    Category.find_each do |category|
+    add category_path(category), :lastmod => category.updated_at, 
+        :priority => 0.9, :changefreq => 'daily'
+    end
   end
 
-  Product.find_each do |product|
+  group(:filename => :products) do
+    Product.find_each do |product|
     add category_product_path(Category.find(product.category_id), product),
-    :lastmod => product.updated_at
+    :lastmod => product.updated_at, :priority => 0.7, :changefreq => 'weekly'
+    end
   end
-
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 end
