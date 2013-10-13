a1c1967e9fc71426e2e6b6c4c31db0c13c5bc4fd
Mark Hoad <markdhoad@gmail.com>
2013-10-13 17:33:32 +0700

    Unscrewing up the CDN fiasco

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Paycheck::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://cdn.fmhgifts.com"
+  # config.action_controller.asset_host = "http://cdn.fmhgifts.com"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
