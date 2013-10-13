1644471456e6347aa89c30ffe143186709ecf80f
Mark Hoad <markdhoad@gmail.com>
2013-10-13 10:40:01 +0700

    Added working image XML sitemap generator for product images

--- config/image_sitemap.rb
+++ config/image_sitemap.rb
@@ -1,26 +1,28 @@
-# SitemapGenerator::Sitemap.create do
-#   group(:filename => :standard_images) do
-#     Product.find_each do |product|
-#       @category = Category.friendly.find(product.category_id)
-#       @product = @category.products.friendly.find(product.id)
-#       add category_product_path(Category.find(product.category_id), product),
-#           :images   => [{
-#             :loc    => @product.picture.url,
-#             :title  => product.name
-#             }]
-#     end
-#   end
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://giftgeek.io"
+SitemapGenerator::Sitemap.create do
+  group(:filename => :standard_images) do
+    Product.find_each do |product|
+      @category = Category.friendly.find(product.category_id)
+      @product = @category.products.friendly.find(product.id)
+      add category_product_path(Category.find(product.category_id), product),
+          :images   => [{
+            :loc    => @product.picture.url,
+            :title  => product.name
+            }]
+    end
+  end
 
-#   group(:filename => :feature_images) do
-#     Product.find_each do |product|
-#       @category = Category.friendly.find(product.category_id)
-#       @product = @category.products.friendly.find(product.id)
-#       add category_product_path(Category.find(product.category_id), product),
-#           :images     => [{
-#             :loc      => @product.social_image.url,
-#             :title    => product.name,
-#             :caption  => product.description
-#             }]
-#     end
-#   end
-# end
+  group(:filename => :feature_images) do
+    Product.find_each do |product|
+      @category = Category.friendly.find(product.category_id)
+      @product = @category.products.friendly.find(product.id)
+      add category_product_path(Category.find(product.category_id), product),
+          :images     => [{
+            :loc      => @product.social_image.url,
+            :title    => product.name,
+            :caption  => product.description
+            }]
+    end
+  end
+end
