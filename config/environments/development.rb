3878c2dbcf029fe3dd17f962172e2ebc7faa42c5
Mark Hoad <markdhoad@gmail.com>
2013-10-13 13:12:03 +0700

    Fixed options in development.rb and production.rb for amazon s3 and paperclip gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Paycheck::Application.configure do
     :url => ':s3_domain_url',
     :path => "images/:category_name/:style/:basename.:extension",
     :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME_DEV'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME_DEV'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Paycheck::Application.configure do
     :url => ':s3_domain_url',
     :path => "images/:category_name/:style/:basename.:extension",
     :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
