38fe13c45d25fdb7521a4f43fce12ce72f54146d
Mark Hoad <markdhoad@gmail.com>
2013-10-30 18:36:36 +0700

    Possible fix to CDN fuck up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Paycheck::Application.configure do
   #Paperclip settings for uploading to S3 in production
   config.paperclip_defaults = {
     :url => ':s3_alias_url',
-    :s3_host_alias => 'cdn%d.fmhgifts.com',
+    :s3_host_alias => "cdn%d.fmhgifts.com",
     :path => "images/:category_name/:style/:basename.:extension",
     :storage => :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
